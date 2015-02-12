03311e5d203fe902d796b6ced78ed53a377a2afc
xloypaypa <245532675@qq.com>
2015-02-12 22:40:38 +0800

    nothing update

--- test/baseTypeTest/Main.java
+++ test/baseTypeTest/Main.java
@@ -1,15 +1,8 @@
 package baseTypeTest;
 
-import baseTool.LT2DType;
 import mathException.TypeBuildException;
-import oneDimensionType.*;
 
 public class Main {
 	public static void main(String[] args) throws TypeBuildException{
-		LTStraight s;
-		LTRay r;
-		r=new LTRay(new LTPoint(0, 0), new LTVector(1, 1));
-		s=new LTStraight(new LTPoint(0, 0), new LTPoint(1, 1));
-		LT2DType c=r.crossPoint(s)[0];
 	}
 }
